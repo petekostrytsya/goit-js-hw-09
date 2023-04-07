3b6335e8bb003f9c2f26241675356a62d8030d6f
petekostrytsya <petekostrytsya@gmail.com>
2023-04-07 23:32:05 +0300

    addTask#1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+let intervalId = null;
+
+startBtn.addEventListener('click', () => {
+    startBtn.setAttribute('disabled', 'disabled');
+    intervalId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+}
+);
+
+stopBtn.addEventListener("click", () => {
+  startBtn.removeAttribute('disabled');
+  clearInterval(intervalId);
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
